61385c0e11fbffdcbb1c631c54f4e362fa6e8839
Ghalia AlHasan <ghaliaalhasan12@gmail.com>
2022-03-28 14:39:03 +0300

    add tweet box contents

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 import './App.css';
+import { useEffect } from 'react';
+import { useState } from 'react';
+
 
 function App() {
+  const [quote, setQuote] = useState('')
+  const [author, setAuthor] = useState('')
+  const url = 'https://api.quotable.io/random'
+  const handleQuote = async () => {
+    const response = await fetch(url)
+    const data = await response.json()
+    setQuote(data?.content)
+    setAuthor(data?.author)
+  }
+  useEffect(() => {
+    handleQuote()
+  }, [])
+
   return (
     <div className="App">
-      Random Quote Machine
-    </div>
+      <div id='quote-box'>
+        <span id='text'>"{quote}"</span>
+        <span id='author'>{author}</span>
+        <button id='new-quote' onClick={handleQuote}>New Quote</button>
+        <a href={`https://twitter.com/intent/tweet?text=${quote}%20-${author}&hashtags=quotes`} id='tweet-quote'>Tweet Quote</a>
+      </div>
+    </div >
   );
 }
 

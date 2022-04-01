fd03259d4efab486e7d1e88f2edb61e7ce519eb1
Ghalia AlHasan <ghaliaalhasan12@gmail.com>
2022-04-01 19:23:05 +0300

    change background and quote color on button click

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import './App.scss';
 import { useEffect } from 'react';
 import { useState } from 'react';
 
@@ -6,23 +6,28 @@ import { useState } from 'react';
 function App() {
   const [quote, setQuote] = useState('')
   const [author, setAuthor] = useState('')
+  const [color, setColor] = useState('')
   const url = 'https://api.quotable.io/random'
-  const handleQuote = async () => {
+
+  const handleClick = async () => {
+    const randomColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`
     const response = await fetch(url)
     const data = await response.json()
     setQuote(data?.content)
     setAuthor(data?.author)
+    setColor(randomColor)
+
   }
   useEffect(() => {
-    handleQuote()
+    handleClick()
   }, [])
 
   return (
-    <div className="App">
+    <div className="App" style={{ backgroundColor: color }}>
       <div id='quote-box'>
-        <span id='text'>"{quote}"</span>
-        <span id='author'>{author}</span>
-        <button id='new-quote' onClick={handleQuote}>New Quote</button>
+        <span id='text' style={{ color: color }}>"{quote}"</span>
+        <span id='author' style={{ color: color }}>- {author}</span>
+        <button id='new-quote' onClick={handleClick}>New Quote</button>
         <a href={`https://twitter.com/intent/tweet?text=${quote}%20-${author}&hashtags=quotes`} id='tweet-quote'>Tweet Quote</a>
       </div>
     </div >
